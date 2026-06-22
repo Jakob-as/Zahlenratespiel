eee711d26fd87ecdbdd9d18b7e586e5c8fb81e14
Jakob-as <jakob.v.pichler@oultook.com>
2026-06-22 13:08:44 +0200

    Versuchs counter

--- Zahlenratespiel.py
+++ Zahlenratespiel.py
@@ -1,11 +1,15 @@
 import random
 geheim = random.randint(1,100)
 tipp = 0
+tries = 0
 while tipp != geheim:
-    tipp = int(input("Errate die Zahl: "))
+    tipp = int(input("Errate die Zahl (1-100): "))
     if tipp < geheim:
         print("Die Zahl ist zu klein")
     elif tipp > geheim:
         print("Die Zahl ist zu groß")
     else:
         print(f"Du hast die Zahl erraten es war: {geheim}")
+    tries += 1
+
+print(f"Es hat {tries} Versuche gebraucht")
